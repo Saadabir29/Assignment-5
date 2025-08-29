ee3be6d3654d367658517902798d41630eec38ff
Saad19720 <siabiruchiha@gmail.com>
2025-08-29 23:13:56 +0600

    DOM and DOM elements

---
+++ script.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const services = [
+    {
+      name: "National Emergency Number",
+      nameEn: "National Emergency",
+      number: "999",
+      category: "All",
+      iconUrl: "./assets/emergency.png",
+    },
+    {
+      name: "Police Helpline Number",
+      nameEn: "Police",
+      number: "999",
+      category: "Police",
+      iconUrl: "./assets/police.png",
+    },
+    {
+      name: "Fire Service Number",
+      nameEn: "Fire Service",
+      number: "999",
+      category: "Fire",
+      iconUrl: "./assets/fire-service.png",
+    },
+    {
+      name: "Ambulance Service",
+      nameEn: "Ambulance",
+      number: "1994-999999",
+      category: "Health",
+      iconUrl: "./assets/ambulance.png",
+    },
+    {
+      name: "Women & Child Helpline",
+      nameEn: "Women & Child Helpline",
+      number: "109",
+      category: "Help",
+      iconUrl: "./assets/logo.png",
+    },
+    {
+      name: "Anti-Corruption Helpline",
+      nameEn: "Anti-Corruption",
+      number: "106",
+      category: "Govt.",
+      iconUrl: "./assets/logo.png",
+    },
+    {
+      name: "Electricity Helpline",
+      nameEn: "Electricity",
+      number: "16216",
+      category: "Electricity",
+      iconUrl: "./assets/logo.png",
+    },
+    {
+      name: "Brac Helpline",
+      nameEn: "Brac",
+      number: "16445",
+      category: "NGO",
+      iconUrl: "./assets/brac.png",
+    },
+    {
+      name: "Bangladesh Railway Helpline",
+      nameEn: "Bangladesh Railway",
+      number: "163",
+      category: "Travel",
+      iconUrl: "./assets/Bangladesh-Railway.png",
+    },
+  ];
